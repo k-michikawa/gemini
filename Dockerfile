5cc212083adc1319179115b01aab48f95549f058
k-michikawa <kazuya.07.mar@gmail.com>
2021-01-06 07:27:45 +0900

    feat jwt

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM ubuntu:18.04 AS development
 
-ARG GPRC_VERSION=1.34.0
-
 ENV CMAKE_INSTALL_DIR /local
 ENV PATH $PATH:/grpc/cmake/build
 ENV PATH $PATH:/grpc/cmake/build/third_party/protobuf
 ENV PATH $PATH:$CMAKE_INSTALL_DIR/bin
+
+WORKDIR /
 
 RUN apt-get update && \
     apt-get install -y \
@@ -21,7 +21,10 @@ RUN apt-get update && \
             pkg-config \
             libtool \
             golang \
-            curl
+            curl \
+            lcov
+
+WORKDIR /
 
 # Install cmake
 RUN wget -q -O cmake-linux.sh https://github.com/Kitware/CMake/releases/download/v3.17.0/cmake-3.17.0-Linux-x86_64.sh && \
@@ -29,16 +32,27 @@ RUN wget -q -O cmake-linux.sh https://github.com/Kitware/CMake/releases/download
     sh cmake-linux.sh -- --skip-license --prefix=$CMAKE_INSTALL_DIR && \
     rm cmake-linux.sh
 
+WORKDIR /
+
 # Install grpc
-RUN git clone --recurse-submodules -b v${GPRC_VERSION} https://github.com/grpc/grpc && \
-    cd grpc && \
+RUN git clone -b v1.34.0 https://github.com/grpc/grpc && \
+    cd /grpc && \
+    git submodule update --init && \
     mkdir -p cmake/build && \
-    pushd cmake/build && \
-    cmake -DgRPC_INSTALL=ON \
-        -DgRPC_BUILD_TESTS=OFF \
-        -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-        ../.. &&\
-    make -j && \
+    cd cmake/build && \
+    cmake ../.. && \
+    make && \
+    make install && \
+    ldconfig 
+
+WORKDIR /
+
+# Install Jwt-cpp
+RUN git clone https://github.com/pokowaka/jwt-cpp.git && \
+    cd jwt-cpp && \
+    mkdir -p release && \
+    cd release && \
+    cmake -DCMAKE_BUILD_TYPE=Release .. && \
     make install
 
 # Build
@@ -49,8 +63,10 @@ RUN rm -f ./pb
 RUN protoc -I proto \
            --grpc_out=./pb \
            --cpp_out=./pb \
-           --plugin=protoc-gen-grpc=`which grpc_cpp_plugin`
-           proto/order.proto
+           --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` \
+           proto/auth.proto
 
 # Make binary
-RUN mkdir build && cd build && cmake .. && make
+# RUN mkdir build && cd build && cmake .. && make
+# Run binary
+# RUN ./build/gemini
--- source/main.cpp
+++ source/main.cpp
@@ -1,24 +1,111 @@
 #include <grpc/grpc.h>
 #include <grpcpp/server_builder.h>
 
+#include <chrono>
+#include <iostream>
 #include <string>
 
-#include "service/service.hpp"
+#include "auth.grpc.pb.h"
+#include "jwt/jwt_all.h"
 
-void run(const std::string &server_address, grpc::Service *service) {
-  grpc::ServerBuilder builder;
+using json = nlohmann::json;
 
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(service);
-  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-  std::cout << "Start gemini on " << server_address << std::endl;
-  server->Wait();
-}
+using grpc::ServerContext;
+using grpc::Status;
+
+using leo::GetJwtRequest;
+using leo::GetJwtResponse;
+using leo::ValidateJwtRequest;
+using leo::ValidateJwtResponse;
+
+class Token {
+ private:
+  char *secret;
+  int expire;
+
+ public:
+  Token(char *s, int e) : secret(s), expire(e) {}
+
+  std::string Generate(const std::string &user_id) {
+    HS256Validator signer(secret);
+    auto now = std::chrono::seconds(std::time(NULL)).count();
+    json payload = {{"sub", "subject"}, {"exp", now + expire}};
+
+    return JWT::Encode(signer, payload);
+  }
+
+  bool Validate(const std::string &token) {
+    ExpValidator exp;
+    HS256Validator signer(secret);
+
+    try {
+      ::json header, payload;
+
+      std::tie(header, payload) = JWT::Decode(token, &signer, &exp);
+      return true;
+    } catch (InvalidTokenError &tfe) {
+      return false;
+    }
+  }
+};
+
+class AuthService final : public leo::AuthService::Service {
+ private:
+  Token &token;
+
+ public:
+  AuthService(Token &t) : token(t) {}
+
+  Status GetJwt(ServerContext *c, const GetJwtRequest *req,
+                GetJwtResponse *res) override {
+    auto jwt = token.Generate(req->userid());
+    res->set_token(jwt);
+    return Status::OK;
+  }
+
+  Status ValidateJwt(ServerContext *c, const ValidateJwtRequest *req,
+                     ValidateJwtResponse *res) override {
+    auto result = token.Validate(req->token());
+
+    if (result) {
+      res->set_newtoken(std::string("hogehoge"));
+      return Status::OK;
+    } else {
+      return Status::CANCELLED;
+    }
+  }
+};
 
 int main(int argc, char **argv) {
-  std::string server_address("0.0.0.0:9020");
-  OrderService::Service service;
+  char *secret;
+  char *expire;
+  char *listen_port;
 
-  run(server_address, &service);
+  secret = std::getenv("SECRET");
+  expire = std::getenv("EXPIRE");
+  listen_port = std::getenv("LISTEN_PORT");
+
+  if (secret == NULL) {
+    throw std::runtime_error("Failed get environment: SECRET");
+  }
+  if (expire == NULL) {
+    throw std::runtime_error("Failed get environment: EXPIRE");
+  }
+  if (listen_port == NULL) {
+    throw std::runtime_error("Failed get environment: LISTEN_PORT");
+  }
+
+  Token token(secret, std::stoi(std::string(expire)));
+  AuthService service(token);
+
+  grpc::ServerBuilder builder;
+
+  builder.AddListeningPort(std::string("0.0.0.0:") + listen_port,
+                           grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Start gemini port: " << listen_port << std::endl;
+  server->Wait();
+
   return 0;
 }
